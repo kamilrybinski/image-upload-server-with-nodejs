5be0ead29da76cd35b04638fca6de3f12900078e
s10570 <s10570@lab7l-7.pjwstk.edu.pl>
2015-01-23 11:28:08 +0100

    serwer

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 /* jshint node: true */
 var app = require("express")();
 var httpServer = require("http").Server(app);
-// var io = require("socket.io)(httpServer);
+var io = require("socket.io")(httpServer);
 
+var static = require('serve-static');
+var less = require('less-middleware');
+var path = require('path');
 var port = process.env.PORT || 3000;
 
+app.use(less(path.join(__dirname, 'public')));
+app.use('/img', static(__dirname + '/public/img'));
+app.use('/js/jquery.min.js', static(__dirname + '/bower_components/jquery/dist/jquery.min.js'));
+app.use('/js/jquery.min.map', static(__dirname + '/bower_components/jquery/dist/jquery.min.map'));
+app.use(static(path.join(__dirname, '/public')));
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello Worldf\n');
-}).listen(3000, '127.0.0.1');
-
-console.log('Serwer HTTP dziala na porcie ' + port);
+httpServer.listen(port, function () {
+	console.log('Serwer HTTP działa na porcie ' + port);
+});
